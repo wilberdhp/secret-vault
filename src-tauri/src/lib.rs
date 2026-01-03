c42c15facf05d5aa30931ef913cf49123bcd0d32
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-03 13:34:04 -0500

    feat: iniciar conexión con la base de datos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
 
 mod db;
 mod models;
@@ -7,10 +7,23 @@ mod commands;
 mod state;
 mod env;
 
+use crate::state::AppState;
+use crate::db::db::init_db;
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let master_password = env::get_master_password();
+
+            let pool = tauri::async_runtime::block_on(
+                init_db(app.app_handle(), master_password)
+            );
+
+            app.manage(AppState::new(pool));
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
