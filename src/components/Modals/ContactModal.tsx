93feef172ef1345603edcad202828913b810da78
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-03 14:21:39 -0500

    refactor: quitar código innecesario

--- src/components/Modals/ContactModal.tsx
+++ src/components/Modals/ContactModal.tsx
@@ -15,9 +15,9 @@ function ContactModal({ getContacts }: ContactModalProps) {
       title={isEditing ? "Editar Contacto" : "Agregar Contacto"}
     >
       <div>
-        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+        {/* <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
           Imagen
-        </label>
+        </label> */}
         <div className="flex items-center space-x-4">
           {contactForm.image ? (
             <img
