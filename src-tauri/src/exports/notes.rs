59f02f862087577d6f70ef781c281933a8c29709
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-10 03:52:59 -0500

    fix: guardar la fecha en formato legible y no en milisegundos

--- src-tauri/src/exports/notes.rs
+++ src-tauri/src/exports/notes.rs
@@ -1,8 +1,16 @@
 use crate::models::notes::NoteDto;
 use std::io::Write;
+use time::{OffsetDateTime, format_description};
 
 pub fn write_notes_txt<W: Write>(writer: &mut W, note: &NoteDto) -> std::io::Result<()> {
-    writeln!(writer, "Fecha: {}\n", note.date)?;
+    
+    let datetime = OffsetDateTime::from_unix_timestamp_nanos((note.date as i128) * 1_000_000).unwrap();
+    let format = format_description::parse("[year]-[month]-[day]")
+        .expect("formato inválido");
+    
+    let date = datetime.format(&format).unwrap();
+    
+    writeln!(writer, "Fecha: {}\n", date)?;
     writeln!(writer, "Título: {}", note.title)?;
     writeln!(writer, "\n{}", note.content)?;
     Ok(())
