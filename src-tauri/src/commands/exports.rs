89741ce1e62a5f623044ca052ecfa5f1de1d046f
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-09 22:17:25 -0500

    feat: agregar función conexión para exportar notas

--- src-tauri/src/commands/exports.rs
+++ src-tauri/src/commands/exports.rs
@@ -1,5 +1,8 @@
 use crate::{commands::contacts::ContactDto2, exports::contacts::write_contacts_vcf};
-use std::{fs::File, io::BufWriter};
+use std::fs::{File, create_dir_all};
+use std::io::BufWriter;
+use crate::models::notes::NoteDto;
+use crate::exports::notes::write_notes_txt;
 
 
 #[tauri::command]
@@ -7,4 +10,18 @@ pub fn export_contacts_vcf_file(path: &str, contacts: &[ContactDto2]) -> Result<
     let file = File::create(path)?;
     let mut writer = BufWriter::new(file);
     Ok(write_contacts_vcf(&mut writer, contacts)?)
+}
+
+#[tauri::command]
+pub fn export_notes_txt_files(dir: &str, notes: &[NoteDto]) -> std::io::Result<()> {
+    create_dir_all(dir)?;
+
+    for (i, note) in notes.iter().enumerate() {
+        let path = format!("{}/note_{}_{}.txt", dir, i + 1, &note.title);
+        let file = File::create(path)?;
+        let mut writer = BufWriter::new(file);
+        write_notes_txt(&mut writer, note)?;
+    }
+
+    Ok(())
 }
