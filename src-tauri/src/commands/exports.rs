e3387fad802dd9c86142187fa425ed3f943878b3
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-10 01:34:26 -0500

    refactor: renombrar funciones de exportación y mejorar la legibilidad del código

--- src-tauri/src/commands/exports.rs
+++ src-tauri/src/commands/exports.rs
@@ -1,25 +1,29 @@
-use zip::{ZipWriter, write::FileOptions};
-use crate::{exports::contacts::write_contacts_vcf};
-use std::fs::{File, create_dir_all};
-use std::io::BufWriter;
+use crate::exports::contacts::write_contacts_vcf;
 use crate::exports::notes::write_notes_txt;
 use crate::exports::passwords::write_passwords_csv;
 use crate::models::users as us;
+use std::fs::{create_dir_all, File};
+use std::io::BufWriter;
+use zip::{write::FileOptions, ZipWriter};
 
 const INVALID_CREDENTIALS: &str = "Credenciales inválidas";
 
 use crate::security;
 use crate::state::AppState;
-use argon2::PasswordVerifier;
 use argon2::password_hash::PasswordHash;
+use argon2::PasswordVerifier;
 
-async fn verify_user_credentials(state: tauri::State<'_, AppState>, id_user: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn verify_user_credentials(
+    state: tauri::State<'_, AppState>,
+    id_user: &str,
+    password: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let account_password = us::find_user_to_verification_password(state.pool(), &id_user)
         .await
         .map_err(|_| INVALID_CREDENTIALS.to_string())?;
 
-    let parsed_hash = PasswordHash::new(&account_password)
-        .map_err(|_| INVALID_CREDENTIALS.to_string())?;
+    let parsed_hash =
+        PasswordHash::new(&account_password).map_err(|_| INVALID_CREDENTIALS.to_string())?;
 
     let argon2 = security::password::argon2_config();
 
@@ -31,8 +35,12 @@ async fn verify_user_credentials(state: tauri::State<'_, AppState>, id_user: &st
 }
 
 #[tauri::command]
-pub async fn export_contacts_vcf_file(state: tauri::State<'_, AppState>, path: &str, id_user: &str, password: &str) -> Result<(), String> {
-    
+pub async fn export_contacts(
+    state: tauri::State<'_, AppState>,
+    path: &str,
+    id_user: &str,
+    password: &str,
+) -> Result<(), String> {
     use crate::models::contacts::get_all;
 
     verify_user_credentials(state.clone(), id_user, password)
@@ -42,18 +50,23 @@ pub async fn export_contacts_vcf_file(state: tauri::State<'_, AppState>, path: &
     let contacts = get_all(&state.pool(), id_user)
         .await
         .map_err(|_| "Error al exportar los contactos".to_string())?;
-    
+
     let file = File::create(path).map_err(|_| "Error interno del sistema".to_string())?;
     let mut writer = BufWriter::new(file);
 
-    write_contacts_vcf(&mut writer, contacts).map_err(|_| "Error interno del sistema".to_string())?;
+    write_contacts_vcf(&mut writer, contacts)
+        .map_err(|_| "Error interno del sistema".to_string())?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub async fn export_notes_txt_files(state: tauri::State<'_, AppState>, path: &str, id_user: &str, password: &str) -> Result<(), String> {
-    
+pub async fn export_notes(
+    state: tauri::State<'_, AppState>,
+    path: &str,
+    id_user: &str,
+    password: &str,
+) -> Result<(), String> {
     const ERROR_MESSAGE: &str = "Error interno del sistema al intentar exportar las notas";
 
     verify_user_credentials(state.clone(), id_user, password)
@@ -79,9 +92,17 @@ pub async fn export_notes_txt_files(state: tauri::State<'_, AppState>, path: &st
 }
 
 #[tauri::command]
-pub async fn export_passwords_csv_file(state: tauri::State<'_, AppState>, path: &str, id_user: &str, password: &str) -> Result<(), String> {
-
+pub async fn export_passwords(
+    state: tauri::State<'_, AppState>,
+    path: &str,
+    id_user: &str,
+    password: &str,
+) -> Result<(), String> {
     const ERROR_MESSAGE: &str = "Error interno del sistema al intentar exportar las contraseñas";
+
+    println!("Id de usuario: {}", id_user);
+    println!("Ruta del archivo: {}", path);
+    println!("Contraseña: {}", password);
 
     verify_user_credentials(state.clone(), id_user, password)
         .await
@@ -91,18 +112,22 @@ pub async fn export_passwords_csv_file(state: tauri::State<'_, AppState>, path: 
 
     let vec_passwords = get_all(&state.pool(), id_user)
         .await
-        .map_err(|_| ERROR_MESSAGE.to_string())?;
+        .map_err(|_| "Error al solicitar los datos en la bd".to_string())?;
 
     let file = File::create(path).map_err(|_| ERROR_MESSAGE.to_string())?;
     let writer = BufWriter::new(file);
-    write_passwords_csv(writer, vec_passwords).map_err(|_| ERROR_MESSAGE.to_string())?;
+    write_passwords_csv(writer, vec_passwords).map_err(|_| "Error al escribir el archivo CSV".to_string())?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub async fn export_zip(state: tauri::State<'_, AppState>, path: &str, id_user: &str, password: &str) -> Result<(), String> {
-
+pub async fn export_zip(
+    state: tauri::State<'_, AppState>,
+    path: &str,
+    id_user: &str,
+    password: &str,
+) -> Result<(), String> {
     const ERROR_MESSAGE: &str = "Error interno del sistema al intentar exportar los datos";
 
     verify_user_credentials(state.clone(), id_user, password)
@@ -112,14 +137,14 @@ pub async fn export_zip(state: tauri::State<'_, AppState>, path: &str, id_user: 
     let file = File::create(path).map_err(|_| ERROR_MESSAGE.to_string())?;
     let mut zip = ZipWriter::new(file);
 
-    let options = FileOptions::default()
-        .compression_method(zip::CompressionMethod::Deflated);
+    let options = FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
 
     // Passwords CSV
     let passwords = crate::models::passwords::get_all(&state.pool(), id_user)
         .await
         .map_err(|_| ERROR_MESSAGE.to_string())?;
-    zip.start_file("passwords.csv", options.clone()).map_err(|_| ERROR_MESSAGE.to_string())?;
+    zip.start_file("passwords.csv", options.clone())
+        .map_err(|_| ERROR_MESSAGE.to_string())?;
     write_passwords_csv(&mut zip, passwords).map_err(|_| ERROR_MESSAGE.to_string())?;
 
     // Contacts VCF
@@ -127,7 +152,8 @@ pub async fn export_zip(state: tauri::State<'_, AppState>, path: &str, id_user: 
         .await
         .map_err(|_| ERROR_MESSAGE.to_string())?;
 
-    zip.start_file("contacts.vcf", options.clone()).map_err(|_| ERROR_MESSAGE.to_string())?;
+    zip.start_file("contacts.vcf", options.clone())
+        .map_err(|_| ERROR_MESSAGE.to_string())?;
     write_contacts_vcf(&mut zip, contacts).map_err(|_| ERROR_MESSAGE.to_string())?;
 
     // Notes TXT
@@ -137,7 +163,8 @@ pub async fn export_zip(state: tauri::State<'_, AppState>, path: &str, id_user: 
 
     for (i, note) in notes.iter().enumerate() {
         let name = format!("notes/note_{}_{}.txt", i + 1, &note.title);
-        zip.start_file(name, options.clone()).map_err(|_| ERROR_MESSAGE.to_string())?;
+        zip.start_file(name, options.clone())
+            .map_err(|_| ERROR_MESSAGE.to_string())?;
         write_notes_txt(&mut zip, note).map_err(|_| ERROR_MESSAGE.to_string())?;
     }
 
